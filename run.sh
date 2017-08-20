95c257be18c1d4dd30c2ed5bffa20d00afcc05a5
r2h2 <rh@identinetics.com>
2017-08-20 22:44:36 +0200

    extend run scripts

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker run -it --rm -v satosa.rh_dev:/opt/satosa/etc --name satosa.rhdev satosa.rhdev $1
+# interactive
+docker run -it --rm -v satosa.rh_dev:/opt/satosa/etc --net=host --name satosa.rhdev satosa.rhdev $1
+
+# background
+# docker run -d --restart=unless-stopped  ---net=host v satosa.rh_dev:/opt/satosa/etc --name satosa.rhdev satosa.rhdev
+
+# --cap-drop=all --cap-add=net_raw
