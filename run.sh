8643b2742737acdfacc5b1582ce50b04c11a2298
r2h2 <rh@identinetics.com>
2017-08-20 23:36:52 +0200

    finalize run script; cap-drop=all

--- run.sh
+++ run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # interactive
-docker run -it --rm -v satosa.rh_dev:/opt/satosa/etc --net=host --name satosa.rhdev satosa.rhdev $1
+#docker run -it --rm -v satosa.rh_dev:/opt/satosa/etc --net=host --cap-drop=all --name satosa.rhdev satosa.rhdev $1
 
 # background
-# docker run -d --restart=unless-stopped  ---net=host v satosa.rh_dev:/opt/satosa/etc --name satosa.rhdev satosa.rhdev
-
-# --cap-drop=all --cap-add=net_raw
+docker run -d --restart=unless-stopped  --net=host --cap-drop=all -v satosa.rh_dev:/opt/satosa/etc --name satosa.rhdev satosa.rhdev
