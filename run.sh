a7ca70a76680c56a94f36349d2d42dc144783114
r2h2 <rainer@hoerbe.at>
2017-08-27 22:51:29 +0200

    run.sh: change docker vol to host dir

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 if [[ $1 == '-i' ]]; then # interactive
-    cmd="docker run -it --rm -v satosa.rh_dev:/opt/satosa/etc --net=host --cap-drop=all --name satosa.rhdev satosa.rhdev $2"
+    docker rm -f satosa.rhdev 2>/dev/null
+    cmd="docker run -it --rm -v /dv/satosa.rhdev:/opt/satosa/etc --net=host --cap-drop=all --name satosa.rhdev satosa.rhdev $2"
     echo $cmd
     $cmd
 else   # background
-    cmd="docker run -d --restart=unless-stopped  --net=host --cap-drop=all -v satosa.rh_dev:/opt/satosa/etc --name satosa.rhdev satosa.rhdev"
+    cmd="docker run -d --restart=unless-stopped  --net=host --cap-drop=all -v /dv/satosa.rhdev:/opt/satosa/etc --name satosa.rhdev satosa.rhdev"
     echo $cmd
     $cmd
 fi
