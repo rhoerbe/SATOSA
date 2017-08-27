4b16c4b5c4b267fe102d507587d381233b88f880
r2h2 <rainer@hoerbe.at>
2017-08-27 22:47:14 +0200

    enhance run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# interactive
-#docker run -it --rm -v satosa.rh_dev:/opt/satosa/etc --net=host --cap-drop=all --name satosa.rhdev satosa.rhdev $1
-
-# background
-docker run -d --restart=unless-stopped  --net=host --cap-drop=all -v satosa.rh_dev:/opt/satosa/etc --name satosa.rhdev satosa.rhdev
+if [[ $1 == '-i' ]]; then # interactive
+    cmd="docker run -it --rm -v satosa.rh_dev:/opt/satosa/etc --net=host --cap-drop=all --name satosa.rhdev satosa.rhdev $2"
+    echo $cmd
+    $cmd
+else   # background
+    cmd="docker run -d --restart=unless-stopped  --net=host --cap-drop=all -v satosa.rh_dev:/opt/satosa/etc --name satosa.rhdev satosa.rhdev"
+    echo $cmd
+    $cmd
+fi
