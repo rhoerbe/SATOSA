695c24e8df70e91f2d912d984eef25e6e2d88055
r2h2 <rainer@hoerbe.at>
2017-10-25 23:01:03 +0200

    get pysaml2==4.5.0 from pypi

--- docker/setup.sh
+++ docker/setup.sh
@@ -5,11 +5,11 @@ pip3 install --upgrade virtualenv
 virtualenv -p python3 /opt/satosa
 /opt/satosa/bin/pip install --upgrade pip setuptools
 
-# pysaml2 4.4.0 not good enough: sigver breaks on diacritic chars in metadata
-cd /src
-git clone https://github.com/rohe/pysaml2
-cd pysaml2
-/opt/satosa/bin/python setup.py install
+# Optional code to get fres version of pysaml2 - not required as 4.5.0 has been pushed to pypi
+#cd /src
+#git clone https://github.com/rohe/pysaml2
+#cd pysaml2
+#/opt/satosa/bin/python setup.py install
 
 /opt/satosa/bin/pip install /src/satosa/
 
