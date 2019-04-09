bc82527957647199fe1200c584e47d17cd3e9a2a
rhoerbe <rainer@hoerbe.at>
2019-04-09 14:58:45 +0200

    do not overwrite existing LANG/LC_ALL to be compatible with centos7

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
 # for Click library to work in satosa-saml-metadata
-export LC_ALL=C.UTF-8
-export LANG=C.UTF-8
-
+if [[ -z "$LC_ALL" ]]; then
+    export LC_ALL=C.UTF-8
+fi
+if [[ -z "$LANG" ]]; then
+    export LANG=C.UTF-8
+fi
 # exit immediately on failure
 set -e
 
