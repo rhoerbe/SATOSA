a76efa3249cd6e0f575e77b639f2b358b1774e82
rhoerbe <rainer@hoerbe.at>
2019-05-29 22:20:53 +0200

    implemented redirecturl microservice (WIP)

--- src/satosa/micro_services/redirect_url.py
+++ src/satosa/micro_services/redirect_url.py
@@ -1,39 +1,66 @@
+"""
+ADFS/SAML-Support for role selection and profile completion after a SAML-Response
+was issued using a redirect-to-idp flow.
+* Store AuthnRequest for later replay
+* Handle redirect-to-idp and replay AuthnRequest after redirect-to-idp flow
+"""
+
 import logging
+import pickle
 import sys
+from base64 import a85encode, a85decode
 import satosa
-from .base import ResponseMicroService
+from .base import RequestMicroService, ResponseMicroService
 
 MIN_PYTHON = (3, 6)
 if sys.version_info < MIN_PYTHON:
     sys.exit("Python %s.%s or later is required.\n" % MIN_PYTHON)
 
+STATE_KEY = "REDIRURLCONTEXT"
 
-class RedirectUrl(ResponseMicroService):
-    """
-    ADFS/SAML-Support for role selection and profile completion after login:
-        if the redirectUrl attribute is set in the response/attribute statement:
-            Store AuthnRequest in SATOSA STATE
-            Redirect to responder
-    3. Re-issue AuthnRequest
-    """
 
+class RedirectUrlRequest(RequestMicroService):
+    """ Store AuthnRequest in SATOSA STATE in case it is required later for the RedirectUrl flow """
     def __init__(self, config: dict, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.endpoint = "/redirecturl_response"
+        logging.info('RedirectUrlRequest microservice active')
+
+    def process(self, context: satosa.context.Context,
+                internal_request: satosa.internal.InternalData) -> satosa.internal.InternalData:
+        logging.debug(f"RedirectUrlRequest: store context (stub)")
+        context_serlzd = a85encode(pickle.dumps(context, pickle.HIGHEST_PROTOCOL))
+        context_serlzd_str = context_serlzd.decode('utf-8')
+        #context.state[STATE_KEY] = context_serlzd_str  # too large for cookie, move to memcached
+        return super().process(context, internal_request)
+
+
+class RedirectUrlResponse(ResponseMicroService):
+    """
+    Handle following events:
+    * Processing a SAML Response:
+        if the redirectUrl attribute is set in the response/attribute statement:
+                        Redirect to responder
+    * Processing a RedirectUrlResponse:
+        Retrieve AuthnRequest in SATOSA STATE
+        Re-issue AuthnRequest
+    """
+    def __init__(self, config: dict, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.endpoint = 'saml2/redirecturl_response'
         self.redir_attr = config['redirect_attr_name']
-        self.STATE_KEY = "REDIRURL"
-        logging.info('RedirectUrl microservice active')
+        self.redir_entityid = config['redir_entityid']
+        logging.info('RedirectUrlResponse microservice active')
 
     def _handle_redirecturl_response(self, context: satosa.context.Context) -> satosa.response.Response:
-        pass
+        logging.debug(f"RedirectUrl microservice: RedirectUrl processing complete")
+        #redir_context = pickle.loads(a85decode(context.state[STATE_KEY]).encode('utf-8'))  # TODO
+        #satosa.base.run(redir_context)
 
     def process(self, context: satosa.context.Context,
                 internal_response: satosa.internal.InternalData) -> satosa.response.Response:
-        redir_state = context.state[self.STATE_KEY]
         if self.redir_attr in internal_response.attributes:
             logging.debug(f"RedirectUrl microservice: Attribute {self.redir_attr} found, starting redirect")
-            context.state[self.STATE_KEY]["authRequest"] = 'placeholder authnRequest'
-            redirecturl = internal_response.attributes[self.redir_attr]
+            redirecturl = internal_response.attributes[self.redir_attr][0] + '?wtrealm=' + 'https%3A%2F%2Fproxy2.test.wpv.portalverbund.at%2Fsp%2Fproxy_backend.xml'
             return satosa.response.Redirect(redirecturl)
         else:
             logging.debug(f"RedirectUrl microservice: Attribute {self.redir_attr} not found")
@@ -41,3 +68,5 @@ class RedirectUrl(ResponseMicroService):
 
     def register_endpoints(self):
         return [("^{}$".format(self.endpoint), self._handle_redirecturl_response), ]
+
+
