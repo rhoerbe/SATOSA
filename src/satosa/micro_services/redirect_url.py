ac8c263a5dacf9d3d912d4ba4991d37872bcde23
rhoerbe <rainer@hoerbe.at>
2019-06-03 22:15:30 +0200

    add local storage (redis) to redirect_url microservice

--- src/satosa/micro_services/redirect_url.py
+++ src/satosa/micro_services/redirect_url.py
@@ -3,14 +3,26 @@ ADFS/SAML-Support for role selection and profile completion after a SAML-Respons
 was issued using a redirect-to-idp flow.
 * Store AuthnRequest for later replay
 * Handle redirect-to-idp and replay AuthnRequest after redirect-to-idp flow
+
+Persist state: Storing the the full context of the AuthnRequest in SATOSA_STATE is not feasible due to cookie size limitations.
+Instead, it is stored in a local redis store, and the key is stored in SATOSA_STATE.
+
+The Redis interface is using a basic implementation creating a connection pool and TCP sockets for each call, which is OK for the modest deployment.
+(Instantiating a global connection pool across gunicorn worker threads would impose some additional complexity.)
+The AuthnRequest is stored unencrypted with the assumption that a stolen request cannot do harm,
+because the final Response will only be delivered to the metadata-specified ACS endpoint.
+
+
 """
 
 import logging
 import pickle
 import sys
-from base64 import a85encode, a85decode
+from typing import Tuple
+import redis
 import satosa
 from .base import RequestMicroService, ResponseMicroService
+from satosa.state import _AESCipher
 
 MIN_PYTHON = (3, 6)
 if sys.version_info < MIN_PYTHON:
@@ -19,42 +31,74 @@ if sys.version_info < MIN_PYTHON:
 STATE_KEY = "REDIRURLCONTEXT"
 
 
+class LocalStore():
+    """ Store context objects in Redis.
+        Create a new key when a new value is set.
+        Delete key/value after reading it
+    """
+    def __init__(self, encryption_key: str):
+        self.redis = redis.Redis(host='localhost', port=6379)
+        self.aes_cipher = _AESCipher(encryption_key)
+
+    def set(self, context: object) -> int:
+        context_serlzd = pickle.dumps(context, pickle.HIGHEST_PROTOCOL)
+        context_enc = self.aes_cipher.encrypt(context_serlzd)
+        key = self.redis.incr('REDIRURL_sequence', 1)
+        self.redis.set(key, context_serlzd, 1800) # generous 30 min timeout to complete SSO transaction
+        return key
+
+    def get(self, key: int) -> object:
+        context_serlzd = self.redis.get(key)
+        self.redis.expire(key, 600)  # delay deletion in case request is repeated due to network issues
+        return pickle.loads(context_serlzd)
+
+
+from autologging import traced
+@traced()
 class RedirectUrlRequest(RequestMicroService):
     """ Store AuthnRequest in SATOSA STATE in case it is required later for the RedirectUrl flow """
     def __init__(self, config: dict, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.local_store = LocalStore(config['db_encryption_key'])
         logging.info('RedirectUrlRequest microservice active')
 
-    def process(self, context: satosa.context.Context,
-                internal_request: satosa.internal.InternalData) -> satosa.internal.InternalData:
+    def process(
+            self, context: satosa.context.Context, internal_request: satosa.internal.InternalData) \
+            -> Tuple[satosa.context.Context, satosa.internal.InternalData]:
+        key = self.local_store.set(context)
+        context.state[STATE_KEY] = str(key)
         logging.debug(f"RedirectUrlRequest: store context (stub)")
-        context_serlzd = a85encode(pickle.dumps(context, pickle.HIGHEST_PROTOCOL))
-        context_serlzd_str = context_serlzd.decode('utf-8')
-        #context.state[STATE_KEY] = context_serlzd_str  # too large for cookie, move to memcached
         return super().process(context, internal_request)
 
 
+@traced()
 class RedirectUrlResponse(ResponseMicroService):
     """
     Handle following events:
     * Processing a SAML Response:
         if the redirectUrl attribute is set in the response/attribute statement:
-                        Redirect to responder
+            Redirect to responder
     * Processing a RedirectUrlResponse:
-        Retrieve AuthnRequest in SATOSA STATE
-        Re-issue AuthnRequest
+        Retrieve previously saved AuthnRequest
+        Replay AuthnRequest
     """
     def __init__(self, config: dict, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.endpoint = 'saml2/redirecturl_response'
+        self.endpoint = 'redirecturl_response'
         self.redir_attr = config['redirect_attr_name']
         self.redir_entityid = config['redir_entityid']
+        self.local_store = LocalStore(config['db_encryption_key'])
         logging.info('RedirectUrlResponse microservice active')
 
-    def _handle_redirecturl_response(self, context: satosa.context.Context) -> satosa.response.Response:
+    def _handle_redirecturl_response(
+            self,
+            context: satosa.context.Context,
+            wsgi_app: callable(satosa.context.Context)) -> satosa.response.Response:
         logging.debug(f"RedirectUrl microservice: RedirectUrl processing complete")
-        #redir_context = pickle.loads(a85decode(context.state[STATE_KEY]).encode('utf-8'))  # TODO
-        #satosa.base.run(redir_context)
+        key = int(context.state[STATE_KEY])
+        authnrequ_context = self.local_store.get(key)
+        resp = wsgi_app.run(authnrequ_context)
+        return resp
 
     def process(self, context: satosa.context.Context,
                 internal_response: satosa.internal.InternalData) -> satosa.response.Response:
